f507f70d490e49c2a1695afd357ca9959f899dd1
Jerome Leon <jerome_leon@yahoo.fr>
2024-03-14 17:35:28 +0100

    Jeu de Memory fonctionnel avec innerText pour exercice

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-const emojis = ["😁","😁","😎","😎","😫","😫","😂","😂","😵","😵","😱","😱","😡","😡","😇","😇","😋","😋","😈","😈"];
-var shuf_emojis = emojis.sort(()=>(Math.random() > .5) ? 2 : -1);
+const emojis = ["🤢","🤢","😎","😎","😭","😭","🤥","🤥","😵","😵","😨","😨","😡","😡","😇","😇","😋","😋","😈","😈"];
+let shuf_emojis = emojis.sort(()=>(Math.random() > 0.5) ? 2 : -1);
+const message = document.querySelector('.victory');
 
 for(var i=0; i<emojis.length;i++){
     let box = document.createElement('div');
@@ -12,22 +13,27 @@ for(var i=0; i<emojis.length;i++){
         this.classList.add('boxOpen')
         setTimeout(function(){
             if(document.querySelectorAll('.boxOpen').length > 1){
-                if(document.querySelectorAll('.boxOpen')[0].innerHTML==document.querySelectorAll('.boxOpen')[1].innerHTML){
+                if(document.querySelectorAll('.boxOpen')[0].innerHTML===document.querySelectorAll('.boxOpen')[1].innerHTML){
                     document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch');
                     document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch');
+                    message.innerText ='👍 Bien joué! 👍';
+                    setTimeout(() => {
+                        message.innerText = '';
+                    }, 4000);
 
                     document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
                     document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
+                    
 
-                    if(document.querySelectorAll('.boxMatch').length==emojis.length){
-                        alert('win');
+                    if(document.querySelectorAll('.boxMatch').length===emojis.length){
+                        message.innerText = "👏 Bravo! C'est gagné 👏";
                     };
                 }else{
                     document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
                     document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
                 };
             }
-        },500);
+        },900);
     };
     document.querySelector('.game').appendChild(box);
 };
