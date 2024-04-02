7a90f85aa20fac30da4b2b9f26c4c3aace2933ef
Jerome Leon <160717180+Jerome-Leon@users.noreply.github.com>
2024-04-02 23:50:47 +0200

    Added a victory message to complete the game

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-
 const emojis = ["🤢","🤢","😎","😎","😭","😭","🤥","🤥","😵","😵","😨","😨","😡","😡","😇","😇","😋","😋","😈","😈"];
-let shuf_emojis = emojis.sort(()=>(Math.random() > 0.5) ? 2 : -1);
-const message = document.querySelector('.victory');
+let shuf_emojis = emojis.sort(() => (Math.random() > 0.5) ? 2 : -1);
+let pairsFound = 0; // Variable pour suivre le nombre de paires trouvées
 
-for(var i=0; i<emojis.length;i++){
+for (var i = 0; i < emojis.length; i++) {
     let box = document.createElement('div');
     box.className = 'item';
     box.innerHTML = shuf_emojis[i];
 
     document.querySelector('.game').appendChild(box);
-    box.onclick = function(){
-        this.classList.add('boxOpen')
-        setTimeout(function(){
-            if(document.querySelectorAll('.boxOpen').length > 1){
-                if(document.querySelectorAll('.boxOpen')[0].innerHTML===document.querySelectorAll('.boxOpen')[1].innerHTML){
+    box.onclick = function () {
+        this.classList.add('boxOpen');
+        setTimeout(function () {
+            if (document.querySelectorAll('.boxOpen').length > 1) {
+                if (document.querySelectorAll('.boxOpen')[0].innerHTML === document.querySelectorAll('.boxOpen')[1].innerHTML) {
                     document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch');
                     document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch');
-                    message.innerText ='👍 Bien joué! 👍';
-                    setTimeout(() => {
-                        message.innerText = '';
-                    }, 4000);
 
                     document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
                     document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
-                    
 
-                    if(document.querySelectorAll('.boxMatch').length===emojis.length){
-                        message.innerText = "👏 Bravo! C'est gagné 👏";
-                    };
-                }else{
+                    pairsFound += 1; // Incrémente le nombre de paires trouvées
+
+                    if (pairsFound === emojis.length / 2) { // Vérifie si toutes les paires ont été trouvées
+                        document.querySelector('.victory').innerText = "🎉 Bravo ! C'est gagné 🎉"; // Affiche le message de victoire
+                    }
+                } else {
                     document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
                     document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
-                };
+                }
             }
-        },900);
+        }, 900);
     };
     document.querySelector('.game').appendChild(box);
 };
